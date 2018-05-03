5d551afd08048c56672781eb62b67d0383d7a15d
MoatazElSakkar <moataz_elsakkar@yahoo.com>
2018-05-04 01:44:04 +0200

    Added Triangle in Dimension.Data

---
+++ Dimension.data/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dimension.data
+{
+    class Triangle
+    {
+        public Point[] Wireframe =new Point[3];
+
+
+        public Triangle(Point a, Point b, Point c)
+        {
+            Wireframe[0] = a;
+            Wireframe[1] = b;
+            Wireframe[2] = c;
+        }
+    }
+}
