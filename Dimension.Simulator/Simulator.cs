f3350ea98b9f846d1e5ecbb01d5e9bfd7cac27ee
Triplem5ds <mmmanany@yahoo.com>
2018-05-05 20:46:57 +0200

    Update Simulator.cs

--- Dimension.Simulator/Simulator.cs
+++ Dimension.Simulator/Simulator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Dimension.data;
 using Dimension.LinearAlgebra;
@@ -25,90 +25,91 @@ namespace Dimension.simulator
     public class Simulator
     {
 
-        public Structure SimulateTransformation(Structure S,Transformation T, params object[] A)
+        public Matrix getRotation(params object[] A)
         {
 
             Matrix multMatrix = new Matrix();
+
+            Angle rotationAngleX = new Angle(float.Parse(A[0].ToString()));
+
+            Angle rotationAngleY = new Angle(float.Parse(A[1].ToString()));
+
+            Angle rotationAngleZ = new Angle(float.Parse(A[2].ToString()));
+
+            float[,] bufferData = new float[4, 4];
+
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    bufferData[i, j] = 0;
+
+            for (int i = 0; i < 4; i++) bufferData[i, i] = 1;
+
+            bufferData[1, 1] = rotationAngleX.cos();
+            bufferData[1, 2] = -rotationAngleX.sin();
+            bufferData[2, 1] = rotationAngleX.sin();
+            bufferData[2, 2] = rotationAngleX.cos();
+
+            Matrix xMat = new Matrix(bufferData);
+
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    bufferData[i, j] = 0;
+
+            for (int i = 0; i < 4; i++) bufferData[i, i] = 1;
+
+            bufferData[0, 0] = rotationAngleY.cos();
+            bufferData[0, 2] = rotationAngleY.sin();
+            bufferData[2, 0] = -rotationAngleY.sin();
+            bufferData[2, 2] = rotationAngleY.cos();
+
+            Matrix yMat = new Matrix(bufferData);
+
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    bufferData[i, j] = 0;
+
+            for (int i = 0; i < 4; i++) bufferData[i, i] = 1;
+
+            bufferData[0, 0] = rotationAngleY.cos();
+            bufferData[0, 1] = -rotationAngleY.sin();
+            bufferData[1, 0] = rotationAngleY.sin();
+            bufferData[1, 1] = rotationAngleY.cos();
+
+            Matrix zMat = new Matrix(bufferData);
+
+            multMatrix = LinearAlgebra.LinearAlgebra.Multiply(xMat, yMat);
+            multMatrix = LinearAlgebra.LinearAlgebra.Multiply(multMatrix, zMat);
+
+            return multMatrix;
+        }
+        public Matrix getScaling(params object[] A)
+        {
+            float scaleX = float.Parse(Convert.ToString(A[0]));
+
+            float scaleY = float.Parse(Convert.ToString(A[1]));
+
+            float scaleZ = float.Parse(Convert.ToString(A[2]));
+
+            float[,] bufferData = new float[4, 4];
+
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    bufferData[i, j] = 0;
+
+            for (int i = 0; i < 4; i++) bufferData[i, i] = 1;
+
+            bufferData[0, 0] = scaleX;
+            bufferData[1, 1] = scaleY;
+            bufferData[2, 2] = scaleZ;
+
+            Matrix multMatrix = new Matrix(bufferData);
+
+            return multMatrix;
+        }
+
+        public Matrix getTranslation(params object[] A)
+        {
             
-            if (T == Transformation.Rotation)
-            {
-                Angle rotationAngleX = new Angle(float.Parse(A[0].ToString()));
-
-                Angle rotationAngleY = new Angle(float.Parse(A[1].ToString()));
-
-                Angle rotationAngleZ = new Angle(float.Parse(A[2].ToString()));
-
-                float[,] bufferData = new float[4,4];
-
-                for (int i = 0; i < 4; i++)
-                    for (int j = 0; j < 4; j++)
-                        bufferData[i, j] = 0;
-
-                for (int i = 0; i < 4; i++) bufferData[i, i] = 1;
-
-                bufferData[1, 1] = rotationAngleX.cos();
-                bufferData[1, 2] = -rotationAngleX.sin();
-                bufferData[2, 1] = rotationAngleX.sin();
-                bufferData[2, 2] = rotationAngleX.cos();
-
-                Matrix xMat = new Matrix(bufferData);
-
-                for (int i = 0; i < 4; i++)
-                    for (int j = 0; j < 4; j++)
-                        bufferData[i, j] = 0;
-
-                for (int i = 0; i < 4; i++) bufferData[i, i] = 1;
-
-                bufferData[0, 0] = rotationAngleY.cos();
-                bufferData[0, 2] = rotationAngleY.sin();
-                bufferData[2, 0] = -rotationAngleY.sin();
-                bufferData[2, 2] = rotationAngleY.cos();
-
-                Matrix yMat = new Matrix(bufferData);
-
-                for (int i = 0; i < 4; i++)
-                    for (int j = 0; j < 4; j++)
-                        bufferData[i, j] = 0;
-
-                for (int i = 0; i < 4; i++) bufferData[i, i] = 1;
-
-                bufferData[0, 0] = rotationAngleY.cos();
-                bufferData[0, 1] = -rotationAngleY.sin();
-                bufferData[1, 0] = rotationAngleY.sin();
-                bufferData[1, 1] = rotationAngleY.cos();
-
-                Matrix zMat = new Matrix(bufferData);
-
-                multMatrix = LinearAlgebra.LinearAlgebra.Multiply(xMat, yMat);
-                multMatrix = LinearAlgebra.LinearAlgebra.Multiply(multMatrix, zMat);
-
-            }
-            else if (T == Transformation.Scaling)
-            {
-
-                float scaleX = float.Parse (Convert.ToString(A[0]));
-
-                float scaleY = float.Parse(Convert.ToString(A[1]));
-
-                float scaleZ = float.Parse(Convert.ToString(A[2]));
-
-                float[,] bufferData = new float[4, 4];
-
-                for (int i = 0; i < 4; i++)
-                    for (int j = 0; j < 4; j++)
-                        bufferData[i, j] = 0;
-
-                for (int i = 0; i < 4; i++) bufferData[i, i] = 1;
-
-                bufferData[0, 0] = scaleX;
-                bufferData[1, 1] = scaleY;
-                bufferData[2, 2] = scaleZ;
-
-                multMatrix = new Matrix(bufferData);
-
-            }
-            else
-            {
                 float translateX = float.Parse(A[0].ToString());
 
                 float translateY = float.Parse(A[1].ToString());
@@ -127,8 +128,27 @@ namespace Dimension.simulator
                 bufferData[1, 3] = translateY;
                 bufferData[2, 3] = translateZ;
 
-                multMatrix = new Matrix(bufferData);
+                Matrix multMatrix = new Matrix(bufferData);
+                return multMatrix;
+        }
 
+        public Structure SimulateTransformation(Structure S, Transformation T, params object[] A)
+        {
+
+
+            Matrix multMatrix = new Matrix();   
+
+            if (T == Transformation.Rotation)
+            {
+                multMatrix = getRotation(A);
+            }
+            else if (T == Transformation.Scaling)
+            {
+                multMatrix = getScaling(A);
+            }
+            else
+            {
+                multMatrix = getTranslation(A);
             }
 
             ///For Every Triangle in the Structre 
@@ -142,15 +162,16 @@ namespace Dimension.simulator
                 {
                     Matrix pointMatrix = new Matrix(Pt);
 
+                    
                     pointMatrix = LinearAlgebra.LinearAlgebra.Multiply(multMatrix, pointMatrix);
 
                     ///Assignment of Points
 
                     Pt.x = pointMatrix.MatData[0,0];
 
-                    Pt.y = pointMatrix.MatData[0,1];
+                    Pt.y = pointMatrix.MatData[1,0];
 
-                    Pt.z = pointMatrix.MatData[0,2];
+                    Pt.z = pointMatrix.MatData[2,0];
 
                 }
             }
