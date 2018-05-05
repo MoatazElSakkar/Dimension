4d54f628c86578a9e1adec67a79bacf23f962092
khaledkee <khaledkee0@gmail.com>
2018-05-05 20:07:44 +0200

    WIP: light source

--- Dimension.Renderer/LightSource.cs
+++ Dimension.Renderer/LightSource.cs
@@ -1,11 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-
 using Dimension.data;
 
-namespace Dimension.Renderer
+namespace Dimension.simulator
 {
-    
+    //Only Logic Light effect is required
+    //which is darkening/brightening the color of the structure depending on it's location from the light source
+    class LightSource
+    {
+        public System.Drawing.Color Intensity;
+        public Point Location;
+
+        public LightSource()
+        {
+            Intensity = System.Drawing.Color.White;
+            Location = new Point(0,0,0);
+        }
+
+        public LightSource(Point location)
+        {
+            Intensity = System.Drawing.Color.White;
+            Location = location;
+        }
+
+        public LightSource(System.Drawing.Color intensity, Point location)
+        {
+            Intensity = intensity;
+            Location = location;
+        }
+
+        public System.Drawing.Color TuneColor(Triangle T, System.Drawing.Color oldColor)
+        {
+            System.Drawing.Color newColor = oldColor;
+            newColor = System.Drawing.Color.FromArgb(oldColor.A, (oldColor.R * Intensity.R + 254) / 255, (oldColor.G * Intensity.G + 254) / 255, (oldColor.B * Intensity.B + 254) / 255);
+            Point normalVector = Point.crossProduct(T.wireframeSegment[0], T.wireframeSegment[1]);
+            Point origin = new Point(0,0,0);
+            Point centerOfGravity = new Point(0,0,0);
+            for(int i = 0;i < 3;i++) centerOfGravity = centerOfGravity + T.wireframeSegment[i];
+            centerOfGravity = centerOfGravity / 3;
+            Point vectorFromLight = Location - centerOfGravity;
+            float cosAngle = Point.dotProduct(vectorFromLight, normalVector) / Point.getDistance(origin, vectorFromLight) / Point.getDistance(origin, normalVector);
+            if(cosAngle < 0) cosAngle = 0;
+            else if(cosAngle > 1) cosAngle = 1;
+            return System.Drawing.Color.FromArgb(newColor.A, (int)Math.Round(newColor.R * cosAngle), (int)Math.Round(newColor.G * cosAngle), (int)Math.Round(newColor.B * cosAngle));
+        }
+
+    }
 }
--- Dimension.data/Point.cs
+++ Dimension.data/Point.cs
@@ -18,6 +18,40 @@ namespace Dimension.data
             return (float) Math.Sqrt(A + B + C);
         }
 
+        public static Point crossProduct(Point a, Point b)
+        {
+            Point c = new Point();
+            c.x = a.y * b.z - a.z * b.y;
+            c.y = a.z * b.x - a.x * b.y;
+            c.z = a.x * b.y - a.y * b.x;
+            return c;
+        }
+
+        public static float dotProduct(Point a, Point b)
+        {
+            return a.x * b.x + a.y * b.y + a.z * b.z;
+        }
+
+        public static Point operator-(Point a, Point b)
+        {
+            return new Point(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        public static Point operator+(Point a, Point b)
+        {
+            return new Point(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        public static Point operator*(Point p, float c)
+        {
+            return new Point(p.x * c, p.y * c, p.z * c);
+        }
+
+        public static Point operator/(Point p, float c)
+        {
+            return new Point(p.x / c, p.y / c, p.z / c);
+        }
+
         public float x, y,z;
 
         public Point(float a, float b,float c)
@@ -25,6 +59,13 @@ namespace Dimension.data
             x = a;
             y = b;
             z = c;
+        }
+
+        public Point()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
         }
     }
 }
